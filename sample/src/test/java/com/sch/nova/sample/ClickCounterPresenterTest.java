12551c07d89a9be2d9ac7e33eed51d673bd19800
sergey <sergey.chelombitko@gmail.com>
2018-03-18 20:06:21 +0300

    JUnit 5

--- sample/src/test/java/com/sch/nova/sample/ClickCounterPresenterTest.java
+++ sample/src/test/java/com/sch/nova/sample/ClickCounterPresenterTest.java
@@ -1,23 +1,23 @@
 package com.sch.nova.sample;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 
-public class ClickCounterPresenterTest {
+class ClickCounterPresenterTest {
     private final ClickCounterView view = mock(ClickCounterView.class);
     private final ClickCounterPresenter presenter = new ClickCounterPresenter();
 
-    @Before
-    public void setUp() {
+    @BeforeEach
+    void setUp() {
         presenter.attachView(view);
     }
 
     @Test
-    public void loadClickCount_callsSetClickCount() {
+    void loadClickCount_callsSetClickCount() {
         presenter.loadClickCount();
 
         verify(view).setClickCount(0);
@@ -25,7 +25,7 @@ public class ClickCounterPresenterTest {
     }
 
     @Test
-    public void buttonClicked_incrementsCountAndCallsSetClickCount() {
+    void buttonClicked_incrementsCountAndCallsSetClickCount() {
         presenter.buttonClicked();
 
         verify(view).setClickCount(1);
